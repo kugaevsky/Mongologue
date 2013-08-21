bfe9e10b70b20cacd1e03797c0739c2c09540793
Nick Kugaevsky <nick@kugaevsky.ru>
2013-08-21 04:09:39 +0400

    Refactor tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,38 +2,32 @@ class TagsController < ApplicationController
   # caches_action :index
 
   def index
-    if !params[:term]
-      @tags = Tag.only(:_id).all
-
-      # Twice faster
-      # @tags = Mongoid.master.collection('tagcloud').find({})
+    @tags = Tag.only(:_id)
+    @tags = if !params[:term]
+      @tags.all
     else
-      @tags = Tag.where(:_id => /^#{params[:term].strip}/).only(:_id).all.to_ary
+      @tags = @tags.where(:_id => /^#{params[:term].strip}/).all
     end
+    tags_ids = @tags.map(&:_id)
 
     respond_to do |format|
-      format.html { render :partial => 'tagscloud'; }
-      format.json { response.headers['Content-Type'] = 'application/json; charset=utf-8';
-                    render :inline => "#{@tags.map(&:id).to_json.html_safe}" }
-      format.xml  { response.headers["Content-Type"] = "application/xml; charset=utf-8";
-                    render :inline => '<?xml version="1.0" encoding="UTF-8" standalone="yes"?> '+
-                                      "<listdata>#{@tags.map(&:id).join('|')}</listdata>" };
-      format.text { render :inline => "#{@tags.map{|m| m['_id']}.join('|')}" };
+      format.html { render :partial => 'tagscloud' }
+      format.json { render json: tags_ids }
+      format.xml  { render xml:  tags_ids }
+      format.text { render text: tags_ids.join('|') }
     end
   end
 
   def show
-    if params[:id]=='*'
-      @tags = Tag.without(:value).all
+    @tags = Tag.without(:value)
+    @tags = if params[:id]=='*'
+      @tags.all
     else
-      @tags = Tag.where(:_id => /#{params[:id]}/).without(:value).all
+      @tags.where(:_id => /#{params[:id]}/).all
     end
     respond_to do |format|
-      format.json { response.headers["Content-Type"] = "application/json; charset=utf-8";
-                    render :inline => "#{@tags.to_set.map(&:id).flatten.to_json.html_safe}" }
-      format.xml { response.headers["Content-Type"] = "application/xml; charset=utf-8";
-                    render :xml => @tags }
+      format.json { render json: @tags.map(&:_id) }
+      format.xml  { render xml:  @tags }
     end
   end
-
 end
